a52b25eba27a25e9976a2f55aca4d404ec96721b
Steve <sorsini4@yahoo.com>
2021-03-09 12:36:34 -0500

    added money spent vs wins graph

--- salaryData.py
+++ salaryData.py
@@ -12,8 +12,8 @@ pd.set_option('display.max_columns', None)
 pd.set_option('chained_assignment', None)
 
 #setting these for later use
-OAKLAND_PRIMARY_COLOR = '#003831'
-OAKLAND_SECONDARY_COLOR = '#EFB21E'
+OAK_PRIMARY_COLOR = '#003831'
+OAK_SECONDARY_COLOR = '#EFB21E'
 STL_PRIMARY_COLOR = '#FF0000'
 STL_SECONDARY_COLOR = '#0000FF'
 
@@ -34,7 +34,7 @@ stl = salaries_teams_years.loc[salaries_teams_years['teamID'] == 'SLN']
 fig, ax = plt.subplots(figsize = (10, 7))
 
 ax.plot(stl['yearID'], stl['salary']/1000000, label = 'STL', color = STL_SECONDARY_COLOR, linewidth = 1.5)
-ax.plot(oak['yearID'], oak['salary']/1000000, label = 'OAK', color = OAKLAND_SECONDARY_COLOR, linewidth = 1.5)
+ax.plot(oak['yearID'], oak['salary']/1000000, label = 'OAK', color = OAK_SECONDARY_COLOR, linewidth = 1.5)
 
 # getting the line data for the Cardinals, A's, and line plot
 # ax.get_lines docs -> https://matplotlib.org/3.1.1/api/_as_gen/matplotlib.axes.Axes.get_lines.html
@@ -52,7 +52,7 @@ x_two, y_two = line_two[:, 0], line_two[:, 1]
 # fill the area underneath the curve for both Cardinals and A's
 # ax.fill_between docs -> https://matplotlib.org/3.1.1/api/_as_gen/matplotlib.axes.Axes.fill_between.html
 ax.fill_between(x_one, y_one, color = STL_PRIMARY_COLOR, alpha = 0.2)
-ax.fill_between(x_two, y_two, color = OAKLAND_PRIMARY_COLOR, alpha = 0.2)
+ax.fill_between(x_two, y_two, color = OAK_PRIMARY_COLOR, alpha = 0.2)
 
 ax.set_xlabel('Year');
 ax.set_ylabel('Team Salary in Millions',);
@@ -70,5 +70,78 @@ plt.show()
 
 #looking at the cardinals top 10 years in payroll 
 print(stl.sort_values(by = 'salary', ascending = False).head(10))
+
+#wins vs money spent: The cardinals obviously spent more than the A's, but did the money spent translate to wins?
+salaries_teams_years = salaries.groupby(['teamID', 'yearID'], as_index = False).sum()
+print(salaries_teams_years.sort_values(by = 'salary', ascending = False).head(10))
+
+#stl_wins = money_wins[(money_wins['teamID'] == 'SLN') & (money_wins['W'] > 90)]
+
+money_wins = salaries_teams_years.merge(teams, on = ['teamID', 'yearID'])
+oak_stl = money_wins[(money_wins['teamID'] == 'OAK') | (money_wins['teamID'] == 'SLN')]
+
+oak_stl['salary/win'] = oak_stl['salary'] / oak_stl['W']
+
+plt.style.use('ggplot')
+
+oak = oak_stl.loc[oak_stl['teamID'] == 'OAK']
+stl = oak_stl.loc[oak_stl['teamID'] == 'SLN']
+
+fig, ax = plt.subplots(figsize = (10, 8))
+
+#setting the s keyword arg to make point size a function of team W's -> the more wins you had as a team, the bigger the point is going to be on the graph
+ax.scatter(x = oak['yearID'], y = oak['salary/win'], color = OAK_PRIMARY_COLOR, alpha = 0.6, edgecolors = OAK_SECONDARY_COLOR, linewidth = 1.5, s = oak['W'] * 1.5)
+ax.scatter(x = stl['yearID'], y = stl['salary/win'], color = STL_PRIMARY_COLOR, alpha = 0.6, edgecolors = STL_SECONDARY_COLOR, linewidth = 1.5, s = stl['W'] * 1.5)
+
+#preventing scientific notation again
+ax.ticklabel_format(useOffset = False)
+
+ax.set_xlim([1985, 2016])
+ax.set_ylim(0)
+ax.set_xlabel('Year')
+ax.set_ylabel('Dollar Spent per Win')
+ax.set_title('Oakland and St. Louis\'s dollars spent per win')
+
+plt.legend(['OAK', 'STL'], loc = 'upper left')
+fig.text(0.8, -.05, 'Point size is a function of team wins', ha = 'center')
+
+plt.show()
+plt.savefig('WinsVsMoneySpent.png')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
